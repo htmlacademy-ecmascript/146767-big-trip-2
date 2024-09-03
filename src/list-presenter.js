841ecc914eea6ff401f8cc68b2612af69c88aebf
Alexander <web-wild-fox@yandex.ru>
2024-09-03 16:52:24 +0400

    Добавляет вывод компонента "Сортировка" на страницу

--- src/list-presenter.js
+++ src/list-presenter.js
@@ -1,10 +1,13 @@
 import ListFilterView from './view/list-filter-view.js';
+import ListSortView from './view/list-sort-view.js';
 import {render} from './render.js';
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
+const sortContainer = document.querySelector('.trip-events');
 
 export default class ListPresenter {
   init() {
     render(new ListFilterView(), filtersContainer);
+    render(new ListSortView(), sortContainer);
   }
 }
