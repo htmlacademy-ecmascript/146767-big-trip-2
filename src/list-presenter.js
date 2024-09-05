c4ae17659bba14ee2e7daadff098f8a4a2720404
Alexander <web-wild-fox@yandex.ru>
2024-09-05 16:34:08 +0400

    Рефакторинг: обновляет вывод компонентов

--- src/list-presenter.js
+++ src/list-presenter.js
@@ -1,22 +1,12 @@
 import ListFilterView from './view/list-filter-view.js';
 import ListSortView from './view/list-sort-view.js';
 import ListContainerView from './view/list-container-view.js';
-import ItemContainerView from './view/item-container-view.js';
 import ItemView from './view/item-view.js';
-import NewPointView from './view/new-point-view.js';
-import NewPointWithoutOffersView from './view/new-point-without-offers-view.js';
-import NewPointWithoutDestinationView from './view/new-point-without-destination-view.js';
 import EditPointView from './view/edit-point-view.js';
 import {render} from './render.js';
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
 const sortContainer = document.querySelector('.trip-events');
-
-const getLastItem = () => {
-  const items = document.querySelectorAll('.trip-events__item');
-
-  return items[items.length - 1];
-};
 
 export default class ListPresenter {
   listComponent = new ListContainerView();
@@ -25,19 +15,10 @@ export default class ListPresenter {
     render(new ListFilterView(), filtersContainer);
     render(new ListSortView(), sortContainer);
     render(this.listComponent, sortContainer);
-    render(new ItemContainerView(), this.listComponent.getElement());
-    render(new EditPointView(), getLastItem());
+    render(new EditPointView(), this.listComponent.getElement());
 
     for (let i = 0; i < 3; i++) {
-      render(new ItemContainerView(), this.listComponent.getElement());
-      render(new ItemView(), getLastItem());
+      render(new ItemView(), this.listComponent.getElement());
     }
-
-    render(new ItemContainerView(), this.listComponent.getElement());
-    render(new NewPointView(), getLastItem());
-    render(new ItemContainerView(), this.listComponent.getElement());
-    render(new NewPointWithoutOffersView(), getLastItem());
-    render(new ItemContainerView(), this.listComponent.getElement());
-    render(new NewPointWithoutDestinationView(), getLastItem());
   }
 }
